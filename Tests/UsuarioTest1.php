c61da34138d234beff1b1edc26d7524a2d51dd5b
gabtmora <gabtmora23@hotmail.com>
2017-03-02 15:56:48 -0500

    Update UsuarioTest1.php

--- Tests/UsuarioTest1.php
+++ Tests/UsuarioTest1.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once('../pages/modelo/usuario/Usuario.php');
+include_once(__DIR__.'/../pages/modelo/usuario/Usuario.php');
 class UsuarioTest1 extends PHPUnit_Framework_TestCase
 {
 
